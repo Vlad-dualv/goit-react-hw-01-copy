0edcbb035592177eac0b55add8f34dcba58bb11b
Vlad <vladswork95@gmail.com>
2024-11-11 18:38:18 +0000

    1st component add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,16 @@
-export default function App() {}
+import Profile from './Profile';
+import userData from '../userData.json';
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+}
